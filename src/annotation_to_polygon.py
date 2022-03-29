112e100b6bd69240540b2049c761b96f423534b0
marleysudbury <marleysudbury@gmail.com>
2022-03-30 00:37:13 +0100

    Limit the number of patches exported from each image, to give a total of 1000 patches for each class (Positive, Negative, Other)

--- src/annotation_to_polygon.py
+++ src/annotation_to_polygon.py
@@ -16,30 +16,54 @@ openslidehome = config['openslide_path']
 os.add_dll_directory(openslidehome)
 import openslide
 
-with open('tumor_001.geojson') as f:
+filename = "22158"
+
+with open('E:\\Training Data !\\Head_Neck_Annotations\\{}.geojson'.format(filename)) as f:
     json_data = json.load(f)
 
 regions = []
+contains_negative = False
 
 for region in json_data['features']:
     json_coords = region['geometry']['coordinates'][0]
     polygon = Polygon(json_coords)
-    regions.append([polygon, region['properties']['classification']['name']])
+    name = region['properties']['classification']['name']
+    regions.append([polygon, name])
+    if name == "Negative":
+        contains_negative = True
 
 
 print(regions)
 
+class_count = {
+    'Negative': 0,
+    'Positive': 0,
+    'Other': 0
+}
+
 slide = openslide.OpenSlide(
-    "E:\\Training Data !\\Cam16\\Training\\Tumor\\tumor_001.tif")
+    "G:\\Data\\Positive\\{}.svs".format(filename))
 
 print(slide.dimensions)
 
 # Iterate over the center point of every 100x100 region of the slide
-for i in range(50, slide.dimensions[0], 100):
-    for j in range(50, slide.dimensions[1], 100):
+for i in range(0, slide.dimensions[0], 100):
+    for j in range(0, slide.dimensions[1], 100):
         point = Point(i, j)
+        patch_class = "Other"
         for region in regions:
             if region[0].contains(point):
-                tile = slide.read_region((i - 50, j - 50), 0, (100, 100))
-                tile.save('E:\\Training Data !\\Cam16Patch\\Training\\{}\\{}_{}_{}.png'.format(
-                    region[1], "tumor_001", i, j))
+                patch_class = region[1]
+
+        if patch_class == "Negative" and class_count[patch_class] < 250 or class_count[patch_class] < 100:
+            tile = slide.read_region((i, j), 0, (100, 100))
+            tile.save('E:\\Training Data !\\Head_Neck_Patch\\{}\\{}_{}_{}.png'.format(
+                patch_class, filename, i, j))
+            class_count[patch_class] += 1
+            print("This patch: {}. P:{}/100 N:{}/250 O:{}/100".format(patch_class,
+                  class_count['Positive'], class_count['Negative'], class_count['Other']))
+            if class_count['Positive'] == class_count['Other'] == 100 and class_count['Negative'] == 250:
+                quit()
+
+            if class_count['Positive'] == class_count['Other'] == 100 and not contains_negative:
+                quit()
